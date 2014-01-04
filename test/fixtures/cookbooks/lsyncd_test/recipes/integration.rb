f7eb748885c7764854d0b8f23ce8f3ff09f12735
Daniel Givens <daniel.givens@rackspace.com>
2014-01-04 17:33:56 -0600

    Added some resources to give lsyncd something to work on

--- test/fixtures/cookbooks/lsyncd_test/recipes/integration.rb
+++ test/fixtures/cookbooks/lsyncd_test/recipes/integration.rb
@@ -1,5 +1,17 @@
 include_recipe 'lsyncd::default'
 
+['/tmp/test1_source', '/tmp/test1_target'].each do |dir|
+  directory dir do
+    mode 0755
+  end
+end
+
+file '/tmp/test1_source/empty_file' do
+  content 'so foo. much bar. wow.'
+  mode 0644
+  action :create
+end
+
 lsyncd_target 'test1' do
   source '/tmp/test1_source'
   target '/tmp/test1_target'
