7cb1dd2f668a549a24a3c3b5a78b45e410d21c52
Daniel Givens <daniel.givens@rackspace.com>
2014-01-17 08:03:27 -0600

    Updated metadata

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,14 @@
 name             'lsyncd'
-maintainer       'Rackspace, Inc.'
+maintainer       'Daniel Givens'
 maintainer_email 'daniel.givens@rackspace.com'
 license          'Apache 2.0'
 description      'Installs/Configures lsyncd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 supports         'ubuntu', '>= 12.04'
 supports         'debian', '>= 7.0'
-supports         'centos', '>= 6.5'
-version          '0.1.1'
+supports         'centos', '>= 6.0'
+supports         'scientific', '>= 6.0'
+supports         'redhat', '>= 6.0'
+supports         'amazon'
+version          '0.2.0'
 depends          'yum', "~> 3.0.0"
