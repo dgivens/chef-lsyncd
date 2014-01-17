bdfcfe327b5d01011ed444312a149cc0f719b128
Daniel Givens <daniel.givens@rackspace.com>
2014-01-16 23:45:45 -0600

    Version bump to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'daniel.givens@rackspace.com'
 license          'Apache 2.0'
 description      'Installs/Configures lsyncd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 depends          'yum', "~> 3.0.0"
