7c7ac05c52a33cf41c6bcb5def5319de42fc4c41
Daniel Givens <daniel.givens@rackspace.com>
2014-10-29 16:55:01 -0500

    Fixed Foodcritic complaints

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@
 # Attributes:: default
 #
 
-default[:lsyncd][:conf_d] = '/etc/lsyncd/conf.d'
-default[:lsyncd][:log_file] = '/var/log/lsyncd.log'
-default[:lsyncd][:status_file] = '/var/log/lsyncd-status.log'
-default[:lsyncd][:interval] = 20
+default['lsyncd']['conf_d'] = '/etc/lsyncd/conf.d'
+default['lsyncd']['log_file'] = '/var/log/lsyncd.log'
+default['lsyncd']['status_file'] = '/var/log/lsyncd-status.log'
+default['lsyncd']['interval'] = 20
--- recipes/default.rb
+++ recipes/default.rb
@@ -17,13 +17,13 @@
 # limitations under the License.
 #
 
-if platform_family?('rhel')
-  yum_repository 'epel' do
-    description 'Extra Packages for Enterprise Linux'
-    mirrorlist 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch'
-    gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6'
-    action :create
-  end
+
+yum_repository 'epel' do
+  description 'Extra Packages for Enterprise Linux'
+  mirrorlist 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch'
+  gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6'
+  action :create
+  only_if { platform_family?('rhel') }
 end
 
 package 'lsyncd'
